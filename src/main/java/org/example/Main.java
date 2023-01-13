e3d105be735e4f769028323cc471f50d4c165c36
djava387 <risad9020@gmail.com>
2023-01-13 21:01:21 +0000

    Tidied-up, Shifted methods to Snap class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,33 +11,6 @@ public class Main {
             snap.playerTurn();
             snap.opponentTurn();
         }
-
-//        CardGame game = new CardGame("Blackjack");
-//
-//
-//        System.out.println("Name of the game: " + game.getName());
-//        System.out.println("\nOriginal deck of cards: ");
-//        printDeck(game.getDeck());
-//
-//        System.out.println("\nDealing a card: " + game.dealCard());
-//
-//        System.out.println("\nDeck of cards after dealing a card: ");
-//        printDeck(game.getDeck());
-//
-//        System.out.println("\nSorting deck of cards in number order: ");
-//        printDeck(game.sortDeckInNumberOrder());
-//
-//        System.out.println("\nSorting deck of cards into suits: ");
-//        printDeck(game.sortDeckIntoSuits());
-//
-//        System.out.println("\nShuffling the deck of cards: ");
-//        printDeck(game.shuffleDeck());
-//    }
-//
-//    public static void printDeck(ArrayList<Card> deck) {
-//        for (Card card : deck) {
-//            System.out.println(card);
-//        }
     }
 }
 
